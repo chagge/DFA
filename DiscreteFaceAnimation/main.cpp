3fadd06af3b2dba10472e4e2db9a81d439e7abee
shunsukesaito <shunsuke.saito16@gmail.com>
2014-04-18 14:21:20 +0900

    Remove small duration on interpolation

--- DiscreteFaceAnimation/main.cpp
+++ DiscreteFaceAnimation/main.cpp
@@ -4,7 +4,8 @@ const std::string movFile="t9_720.avi";
 const std::string ymlFile="CommonPoints.yml";
 const std::string output="result.avi";
 
-const cv::Rect rect(535,260,146,100);
+//const cv::Rect rect(535,260,146,100);
+const cv::Rect rect(517,156,180,208);
 const int candiSize=10;
 
 GENEFACE geneface(movFile,ymlFile,candiSize);
